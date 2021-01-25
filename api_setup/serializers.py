07157368befd594507a4ec29ad172430127ca504
Ronard Luna <ronardevelops@gmail.com>
2021-01-25 16:05:39 -0500

    Working with model serializer

--- api_setup/serializers.py
+++ api_setup/serializers.py
@@ -1,21 +1,27 @@
 from rest_framework import serializers
 from .models import Article
 
-class ArticleSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length= 100)
-    body = serializers.CharField()
-    author = serializers.CharField(max_length= 100)
-    email = serializers.EmailField(max_length=100)
-    date = serializers.DateField()
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ['id', 'title', 'body' ,'author', 'email' ,'date']
+  
 
-    def create(self, validated_data):
-        return Article.objects.create(validated_data)
+# class ArticleSerializer(serializers.Serializer):
+#     title = serializers.CharField(max_length= 100)
+#     body = serializers.CharField()
+#     author = serializers.CharField(max_length= 100)
+#     email = serializers.EmailField(max_length=100)
+#     date = serializers.DateField()
+
+#     def create(self, validated_data):
+#         return Article.objects.create(validated_data)
     
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.body = validated_data.get('body', instance.body)
-        instance.author = validated_data.get('author', instance.author)
-        instance.email = validated_data.get('email', instance.email)
-        instance.date = validated_data.get('date', instance.date)
-        instance.save()
-        return instance
+#     def update(self, instance, validated_data):
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.body = validated_data.get('body', instance.body)
+#         instance.author = validated_data.get('author', instance.author)
+#         instance.email = validated_data.get('email', instance.email)
+#         instance.date = validated_data.get('date', instance.date)
+#         instance.save()
+#         return instance
